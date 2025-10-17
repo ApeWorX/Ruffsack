5c66f9438a939397ea83098c67a8a878e7f23c75
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2025-10-17 16:08:13 -0400

    feat(Contract): refactor `execute` to support native multicall (#4)

--- ruffsack/messages/__init__.py
+++ ruffsack/messages/__init__.py
@@ -1,7 +1,7 @@
 from .admin import ActionType
-from .execute import create_def as create_execute_def
+from .execute import Execute
 
 __all__ = [
     ActionType.__name__,
-    "create_execute_def",
+    Execute.__name__,
 ]
--- ruffsack/messages/execute.py
+++ ruffsack/messages/execute.py
@@ -1,24 +1,41 @@
-from ape.types.address import AddressType
-from eip712 import EIP712Message
-from packaging.version import Version
+from typing import TYPE_CHECKING, Self
+
+from ape.utils import ManagerAccessMixin
+from eip712 import EIP712Message, EIP712Type
+
+if TYPE_CHECKING:
+    from ape.types import AddressType
+    from packaging.version import Version
 
 
-# TODO: Refactor this to a multicall-like object
-class ExecuteBase(EIP712Message):
-    _name_ = "Ruffsack Wallet"
+class Call(EIP712Type):
     target: "address"  # type: ignore
     value: "uint256"  # type: ignore
     data: "bytes"
 
 
-def create_def(
-    version: Version,
-    address: AddressType,
-    chain_id: int,
-) -> type[ExecuteBase]:
-    class Execute(ExecuteBase):
-        _verifyingContract_ = address
-        _version_ = str(version)
-        _chainId_ = chain_id
+class ExecuteBase(EIP712Message):
+    _name_ = "Ruffsack Wallet"
 
-    return Execute
+    calls: list[Call] = []
+
+
+class Execute(ManagerAccessMixin):
+    def __init__(self, version: "Version", address: "AddressType", chain_id: int):
+        class Execute(ExecuteBase):
+            _verifyingContract_ = address
+            _version_ = str(version)
+            _chainId_ = chain_id
+
+        self.message = Execute()
+
+    def add_raw(self, target: "AddressType", value: int = 0, data: bytes = b"") -> Self:
+        self.message.calls.append(Call(target=target, value=value, data=data))
+        return self
+
+    def add(self, call, *args, value: int = 0) -> Self:
+        return self.add_raw(
+            target=call.contract.address,
+            value=value,
+            data=call.encode_input(*args),
+        )
